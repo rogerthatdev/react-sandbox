061043684b5916d97eaaa1cd50184e514c93bb7c
Roger Martinez <roger.that.dev@gmail.com>
2024-03-10 11:46:37 -0700

    empty components

---
+++ example/src/app/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <main>
+        <h1>footer!</h1>
+      </main>
+    );
+  }
+  
---
+++ example/src/app/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <main>
+        <h1>header!</h1>
+      </main>
+    );
+  }
+  

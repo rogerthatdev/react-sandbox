c89db7778f2e6638fcae69397f35d17809331f3e
Roger Martinez <roger.that.dev@gmail.com>
2024-03-11 16:34:12 -0700

    add custom fonts

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* Use Inter to use font across entire body element. 'antialiased' uses Tailwind to smooth out the font */}
+      <body className={`${inter.className} antialiased`} >{children}</body>
     </html>
   );
 }
